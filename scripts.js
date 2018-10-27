7dffb63b5005713dd86386720edb43449756087a
arnonerba <arnon@arnonerba.com>
2018-10-26 23:51:39 -0700

    Add unminified scripts file to repo

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// Minified by javascript-minifier.com
+jQuery(document).ready(function() {
+	jQuery( "i.shownav" ).click(function(event) {
+		jQuery( "#navdrawer" ).toggleClass( "navdraweropen" );
+		jQuery( "#scrim" ).toggleClass( "visible" );
+		jQuery(document.body).toggleClass( "noscroll" );
+		event.stopPropagation();
+	});
+    jQuery( "#scrim" ).click(function(event) {  
+		jQuery( "#navdrawer" ).removeClass( "navdraweropen" );
+		jQuery( "#scrim" ).removeClass( "visible" );
+		jQuery(document.body).removeClass( "noscroll" );
+    });
+	jQuery( "pre" ).each(function(i, block) {
+		hljs.highlightBlock(block);
+	});
+});
+jQuery(window).scroll(function(){
+	if(jQuery(document).scrollTop() > 0) {
+		jQuery("#header").addClass("scrolled");
+	}
+	else {
+		jQuery("#header").removeClass("scrolled");
+	}
+});
