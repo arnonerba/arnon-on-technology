b6dbaf30a0223bda444fc2aa8879fb78317ec6d9
arnonerba <arnon@arnonerba.com>
2018-10-26 23:58:37 -0700

    Include improved emoji removal code

--- functions.php
+++ functions.php
@@ -45,6 +45,7 @@ function disable_emojis() {
 	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );	
 	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
 	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+	add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
 }
 add_action( 'init', 'disable_emojis' );
 
@@ -54,4 +55,16 @@ function disable_emojis_tinymce( $plugins ) {
 	} else {
 		return array();
 	}
+}
+
+function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+	if ( 'dns-prefetch' == $relation_type ) {
+		$emoji_svg_url_bit = 'https://s.w.org/images/core/emoji/';
+		foreach ( $urls as $key => $url ) {
+			if ( strpos( $url, $emoji_svg_url_bit ) !== false ) {
+				unset( $urls[$key] );
+			}
+		}
+	}
+	return $urls;
 }
