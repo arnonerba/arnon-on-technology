14f9c85d90b76dc7c833e2f93146635cea07ba4d
arnonerba <arnon@arnonerba.com>
2018-12-08 22:04:44 -0800

    Clean up spacing in PHP

--- single.php
+++ single.php
@@ -11,22 +11,22 @@
 				<h1><?php the_title(); ?></h1>
 			</div>
 			<div class="cardsubtitle">
-				<h2>Posted on <?php echo get_the_date(); ?> by <?php the_author_link(); ?> in <?php $categories = get_the_category(); $cat = $categories[0]; echo '<a href="'.get_category_link($cat).'">'.$cat->name.'</a>'; ?></h2>
+				<h2>Posted on <?php echo get_the_date(); ?> by <?php the_author_link(); ?> in <?php $categories = get_the_category(); $cat = $categories[0]; echo '<a href="'.get_category_link( $cat ).'">'.$cat->name.'</a>'; ?></h2>
 			</div>
 			<div class="cardtext full">
 				<?php the_content(); ?>
 			</div>
 			<div class="cardactions">
-				<div class="cardtags">Tagged: <?php foreach( get_the_tags() as $tag ) { echo '<a href="'.get_tag_link($tag).'">'.'#'.str_replace(" ", "-", strtolower($tag->name)).'</a>'. ' '; } ?></div>
+				<div class="cardtags">Tagged: <?php foreach( get_the_tags() as $tag ) { echo '<a href="'.get_tag_link( $tag ).'">'.'#'.str_replace( " ", "-", strtolower( $tag->name ) ).'</a>'. ' '; } ?></div>
 			</div>
 		</article>
 	<?php endwhile; ?>
 	<?php endif; ?>
 
 <div id="page_navigation">
-	<?php next_post_link('%link', 'Newer Post', FALSE); ?>
-	<a href="<?php bloginfo('url'); ?>">Home</a>
-	<?php previous_post_link('%link', 'Older Post', FALSE); ?>
+	<?php next_post_link( '%link', 'Newer Post', FALSE ); ?>
+	<a href="<?php bloginfo( 'url' ); ?>">Home</a>
+	<?php previous_post_link( '%link', 'Older Post', FALSE ); ?>
 </div>
 
 <?php get_footer(); ?>
