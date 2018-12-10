9f14f75b3e6580b1602c185618a5fd8154933d6a
arnonerba <arnon@arnonerba.com>
2018-12-09 22:40:49 -0800

    Remove unnecessary CSS class

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <?php ?>
 
 <form id="searchform" role="search" method="get" action="<?php echo esc_url( bloginfo( 'url' ) ); ?>">
-	<input class="textfield" type="search" placeholder="Search this blog" name="s">
+	<input type="search" placeholder="Search this blog" name="s">
 	<button class="button iconbutton" type="submit"><i class="material-icons">search</i></button>
 </form>
