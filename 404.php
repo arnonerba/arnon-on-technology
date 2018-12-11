86facd3813c7992fb1a28828c08aaa2328e7c839
arnonerba <arnon@arnonerba.com>
2018-12-10 23:07:28 -0800

    Clean up spacing in PHP

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
 <div class="error">
 	<span id="shruggie">¯\_(ツ)_/¯</span>
 	<p>Uh oh, we can't find that page.</p>
-	<a href="<?php bloginfo('url'); ?>" class="button">Home</a>
+	<a href="<?php bloginfo( 'url' ); ?>" class="button">Home</a>
 </div>
 
 <?php get_footer(); ?>
