67ab85630b69c74faacdd363851f8d8a2bf4223c
arnonerba <arnon@arnonerba.com>
2018-12-16 13:26:41 -0800

    Simplify add_theme_support code

--- functions.php
+++ functions.php
@@ -1,13 +1,8 @@
 <?php
 
 add_theme_support( 'post-thumbnails' );
-
 add_theme_support( 'automatic-feed-links' );
-
-function add_title_support() {
-	add_theme_support( 'title-tag' );
-}
-add_action( 'after_setup_theme', 'add_title_support' );
+add_theme_support( 'title-tag' );
 
 function the_modified_title( $title ) {
 	$title['tagline'] = null;
