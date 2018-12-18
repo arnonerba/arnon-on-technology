e1bbf00185d12406cc8fa568608acf21a73fe426
Arnon Erba <arnon@arnonerba.com>
2018-12-17 23:09:41 -0800

    Add indentation

--- 404.php
+++ 404.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
 
-<div class="card">
-	<div class="cardtitle"><h1>404: Page not found</h1></div>
-	<div class="cardtext full"></div>
-</div>
+	<div class="card">
+		<div class="cardtitle"><h1>404: Page not found</h1></div>
+		<div class="cardtext full"></div>
+	</div>
 
-<div class="error">
-	<span id="shruggie">¯\_(ツ)_/¯</span>
-	<p>Uh oh, we can't find that page.</p>
-	<a href="<?php bloginfo( 'url' ); ?>" class="button">Home</a>
-</div>
+	<div class="error">
+		<span id="shruggie">¯\_(ツ)_/¯</span>
+		<p>Uh oh, we can't find that page.</p>
+		<a href="<?php bloginfo( 'url' ); ?>" class="button">Home</a>
+	</div>
 
 <?php get_footer(); ?>
