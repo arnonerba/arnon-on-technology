f0259a5f81eed21daea6e6275ed40e2d351f71fd
arnonerba <arnon@arnonerba.com>
2018-12-17 22:15:16 -0800

    Improve indentation and spacing

--- footer.php
+++ footer.php
@@ -1,15 +1,10 @@
-</main>
-
-<footer id="footer">
-	<div class="footertext">&copy; Arnon Erba <?php echo date('Y'); ?></div>
-</footer>
-
-</div>
-
-<div id="scrim"></div>
-
+		</main>
+		<footer id="footer">
+			<div class="footertext">&copy; Arnon Erba <?php echo date('Y'); ?></div>
+		</footer>
+	</div>
+	<div id="scrim"></div>
 <?php wp_footer(); ?>
-
 </body>
 
 </html>
