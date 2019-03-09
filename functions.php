eb85b3c006a0bc3ac6df12c8fe41ac47b538e038
Arnon Erba <arnon@arnonerba.com>
2019-03-09 12:07:46 -0800

    Refactor code for disabling emojis

--- functions.php
+++ functions.php
@@ -47,7 +47,10 @@ function arnon_on_technology_enqueue_scripts_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'arnon_on_technology_enqueue_scripts_styles' );
 
-function disable_emojis() {
+function arnon_on_technology_disable_emojis() {
+	/*
+	Disable additional emoji-related scripts and content loaded by WordPress. (Originally by Ryan Hellyer).
+	*/
 	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 	remove_action( 'wp_print_styles', 'print_emoji_styles' );
@@ -55,20 +58,14 @@ function disable_emojis() {
 	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
 	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );	
 	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
-	add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
+	add_filter( 'wp_resource_hints', 'arnon_on_technology_remove_emojis_dns_prefetch', 10, 2 );
 }
-add_action( 'init', 'disable_emojis' );
+add_action( 'init', 'arnon_on_technology_disable_emojis' );
 
-function disable_emojis_tinymce( $plugins ) {
-	if ( is_array( $plugins ) ) {
-		return array_diff( $plugins, array( 'wpemoji' ) );
-	} else {
-		return array();
-	}
-}
-
-function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+function arnon_on_technology_remove_emojis_dns_prefetch( $urls, $relation_type ) {
+	/*
+	Remove emoji CDN hostname from DNS prefetching hints.
+	*/
 	if ( 'dns-prefetch' == $relation_type ) {
 		$emoji_svg_url_bit = 'https://s.w.org/images/core/emoji/';
 		foreach ( $urls as $key => $url ) {
