8ce437eedb0ba6af6bd2c47fcd47f697e894a371
Arnon Erba <arnon@arnonerba.com>
2019-03-17 11:50:25 -0700

    Fix spacing for consistency

--- scripts.js
+++ scripts.js
@@ -1,25 +1,24 @@
 // Minified by javascript-minifier.com
 jQuery(document).ready(function() {
-	jQuery( "i.shownav" ).click(function(event) {
-		jQuery( "#navdrawer" ).toggleClass( "navdraweropen" );
-		jQuery( "#scrim" ).toggleClass( "visible" );
-		jQuery(document.body).toggleClass( "noscroll" );
+	jQuery("i.shownav").click(function(event) {
+		jQuery("#navdrawer").toggleClass("navdraweropen");
+		jQuery("#scrim").toggleClass("visible");
+		jQuery(document.body).toggleClass("noscroll");
 		event.stopPropagation();
 	});
-    jQuery( "#scrim" ).click(function(event) {  
-		jQuery( "#navdrawer" ).removeClass( "navdraweropen" );
-		jQuery( "#scrim" ).removeClass( "visible" );
-		jQuery(document.body).removeClass( "noscroll" );
+    jQuery("#scrim").click(function(event) {  
+		jQuery("#navdrawer").removeClass("navdraweropen");
+		jQuery("#scrim").removeClass("visible");
+		jQuery(document.body).removeClass("noscroll");
     });
-	jQuery( "pre" ).each(function(i, block) {
+	jQuery("pre").each(function(i, block) {
 		hljs.highlightBlock(block);
 	});
 });
-jQuery(window).scroll(function(){
+jQuery(window).scroll(function() {
 	if(jQuery(document).scrollTop() > 0) {
 		jQuery("#header").addClass("scrolled");
-	}
-	else {
+	} else {
 		jQuery("#header").removeClass("scrolled");
 	}
 });
